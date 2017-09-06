9ab93e18b69fb3440a3fb7c4f3a99d73ba1a6a81
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2017-09-06 16:57:54 +0200

    Added a way to return a default value other than 0

--- filters/filter_get_value.erl
+++ filters/filter_get_value.erl
@@ -5,17 +5,19 @@
 -module(filter_get_value).
 
 -export([
-    get_value/5
+    get_value/5,
+    get_value/6
 ]).
 
 -include_lib("zotonic.hrl").
 
+get_value(Values, Tick, XProp, YProp, Context) ->
+    get_value(Values, Tick, XProp, YProp, 0, Context).
 
-get_value(Values, Tick, XProp, YProp, _Context) ->
-    ?DEBUG({Tick, Values, XProp, YProp}),
+get_value(Values, Tick, XProp, YProp, Default, _Context) ->
     case find(Values, {XProp, Tick}) of
-        undefined -> 0;
-        PropList -> proplists:get_value(YProp, PropList, 0)
+        undefined -> Default;
+        PropList -> proplists:get_value(YProp, PropList, Default)
     end.
 
 find([], _) -> undefined;
